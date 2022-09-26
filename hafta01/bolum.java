e3af941deb7f1cd3c17a391f563c1b28586ba016
Yasin Hoca <myasinozsaglam@gmail.com>
2022-09-26 12:16:30 +0300

    Add files via upload

---
+++ hafta01/bolum.java
@@ -0,0 +1,9 @@
+package hafta01;
+
+public class bolum extends okul{
+    public String bolum_ismi;
+    protected int ogrenci_sayisi;
+    protected void ogrenci_ekle(){
+        //yapılacak isler
+    }
+}
---
+++ hafta01/okul.java
@@ -0,0 +1,8 @@
+package hafta01;
+
+public class okul {
+    public String isim;
+    public String adress;
+}
+
+
